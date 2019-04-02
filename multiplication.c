8d66913cb7b53fe6de3a0a1c6fe33f96a9489b52
Sagar <49182410+sagarprasad007@users.noreply.github.com>
2019-04-02 12:12:33 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int a, 
+	printf("Enter a number : ");
+	scanf("%d", &n);
+	printf("\nMultiplication table of %d is =\n");
+	for(i=1; i<=12; i++)
+	{
+		s=n*i;
+	}
+	printf("%d X %d = %d",n, i, s);
+} 
